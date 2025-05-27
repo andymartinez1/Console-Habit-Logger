4e4f06298d812e5b906de392b86a2b8533458b80
Andrew Martinez <andy72391@gmail.com>
2025-05-27 00:31:21 -0400

    Beginning of a console habit logger app using SQLite.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.Sqlite;
+
+string connectionString = "Data Source=habbitlogger.db";
+
+using (var connection = new SqliteConnection(connectionString))
+{
+    connection.Open();
+
+    // Create a table if it doesn't exist
+    var createTableCommand = connection.CreateCommand();
+
+    createTableCommand.CommandText = 
+        @"CREATE TABLE IF NOT EXISTS habits (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Name TEXT NOT NULL,
+            Description TEXT,
+            Quantity INTEGER,
+            Date TEXT NOT NULL
+            )";
+
+    createTableCommand.ExecuteNonQuery();
+
+    connection.Close();
+}
